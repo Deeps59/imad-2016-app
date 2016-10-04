a87191282ada67cec01e860c3e60e6698cc2877d
Deeps59 <deepak2375@gmail.com>
2016-10-05 01:00:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,65 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-//lines 8,12 and 16 handling specific urls
+
+
+var articleOne={
+    title:'Article One|Deepak Saidam',
+    heading: 'Article One',
+    date: 'Sept 5 2016',
+    content:
+        `<div>
+            <p>
+                This is the content of the first ariticle. IN this article we've created the html file with basic commands in html.
+            </p>
+            <p>
+                This is the second paragraph of the article.
+            </p>
+        </div>`
+};
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    var htmltemplate=`
+                    <html>
+                        <head>
+                            <title>
+                                ${title}
+                            </title>
+                            <meta name="viewport" content="width=device-width, intial-scale=1"/>
+                            <link href="/ui/style.css" rel="stylesheet" />
+                        </head>
+                        <body>
+                            <div class="container">
+                            <div>
+                                <a href="/">Home</a>
+                            </div>
+                            <hr/>
+                            <h3>
+                                ${heading}
+                            </h3>
+                            <div>
+                                ${date}
+                            </div>
+                            <div>
+                                ${content}
+                            </div>
+                            </div>
+                        </body>
+                    </html>`
+                    ;
+return htmltemplate;
+}
+
+//lines with app.get handling specific urls
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article_one',function(req,res) {
-    res.sendFile(path.join(__dirname,'ui' , 'article_one.html'));
+    res.send(createTemplate(articleOne));
 });
 app.get('/article_two',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article_two.html'));
